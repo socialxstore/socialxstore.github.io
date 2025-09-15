ae6c08098707c811fda67803d281fb2d5a0dd423
socialxstore <bilal.moist@gmail.com>
2025-09-15 22:13:18 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Simple example: animate FAQ questions on click to show answers
-document.querySelectorAll('.faq-item h3').forEach(header => {
-  header.addEventListener('click', () => {
-    const answer = header.nextElementSibling;
-    answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
-  });
-});
